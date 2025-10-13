07e45dced59628e1bebf65fbd1bdb0b1fc30ecfa
hungdoanbeobeo <23001528@hus.edu.vn>
2025-10-13 08:14:30 +0700

    Update semigroup.pl

--- Chapter 5/semigroup.pl
+++ Chapter 5/semigroup.pl
@@ -1,1 +1,16 @@
 % Create a solution for Exercise 5.1, page 88. More precisely, you will attempt to prove the equality of left- and right-neutral elements of semigroups with PROLOG and fail. Try to explain why?
+
+
+m(X, Y).
+assoc(X, Y, Z) :-
+    m(m(X, Y), Z) = m(X, m(Y, Z)).
+left_neutral(E_l, X) :-
+    m(E_l, X) = X.
+
+right_neutral(E_r, X) :-
+    m(X, E_r) = X.
+
+prove_equal(E_l, E_r) :-
+    left_neutral(E_l, X),
+    right_neutral(E_r, X),
+    E_l = E_r.
